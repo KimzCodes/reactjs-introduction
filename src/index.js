88a8683c7963064003bf50586231cc0188c67fd6
kareem nour <kimzcodes@gmail.com>
2021-07-19 16:09:01 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDom from "react-dom";
-import App from "./components/App/App";
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './App/App';
 
-ReactDom.render(<App />, document.getElementById("root"));
+ReactDom.render(<App />, document.getElementById('root'));
